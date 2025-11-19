db0075635676a37de84528e0826f02ff72cd7cd0
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-11-19 16:26:20 +0100

    feat: Update header text to specify local SQL connection

--- demoapps/04/MyAppName/src/App.tsx
+++ demoapps/04/MyAppName/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Power SDK + SQL Connection</h1>
+      <h1>Power SDK + SQL Connection (local)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
