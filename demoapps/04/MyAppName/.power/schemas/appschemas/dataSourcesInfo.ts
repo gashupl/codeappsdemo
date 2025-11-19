7618ada3654d801e12902bb2bcd02142fe7542d9
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-11-19 16:01:37 +0100

    feat: Add Payment schema, model, service, and data source configuration

---
+++ demoapps/04/MyAppName/.power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "payment": {
+    "tableId": "Payment",
+    "version": "v2",
+    "primaryKey": "payment_id",
+    "dataSourceType": "Connector",
+    "apis": {}
+  }
+};
